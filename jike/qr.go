47cf47582433902389172bda598a6c8837fad4a6
cypggs <qcypggs@qq.com>
2019-03-20 16:31:33 +0800

    Update qr.go

--- jike/qr.go
+++ jike/qr.go
@@ -8,7 +8,7 @@ import (
 func GenerateQRCode(uuid string) {
 	url := `jike://page.jk/web?url=https%3A%2F%2Fruguoapp.com%2Faccount%2Fscan%3Fuuid%3D` + uuid + `&displayHeader=false&displayFooter=false`
 	config := qrterminal.Config{
-		Level:     qrterminal.M,
+		Level:     qrterminal.S,
 		Writer:    os.Stdout,
 		BlackChar: qrterminal.BLACK,
 		WhiteChar: qrterminal.WHITE,
